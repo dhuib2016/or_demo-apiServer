5b811ddc2ab0b7a47bcc7d66d72c8ec6371f7d02
tweyseo <tweyseo@gmail.com>
2018-01-05 10:18:45 +0800

    fix err handler

--- app/modules/test/errHandler.lua
+++ app/modules/test/errHandler.lua
@@ -1,4 +1,5 @@
--- simple error handler ignore req, res, next
-return function(err)
-    ngx.log(ngx.ERR, "/test err: ", err)
+return function()
+	return function(err)
+		ngx.log(ngx.ERR, "/test err: ", err)
+	end
 end
--- app/modules/user/errHandler.lua
+++ app/modules/user/errHandler.lua
@@ -1,4 +1,5 @@
--- simple error handler ignore req, res, next
-return function(err)
-    ngx.log(ngx.ERR, "/user err: ", err)
+return function()
+	return function(err)
+		ngx.log(ngx.ERR, "/user err: ", err)
+	end
 end
