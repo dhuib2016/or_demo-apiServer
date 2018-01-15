8d7f2ad2d4c42df240d6538189d06e18acbd1cc7
tweyseo <tweyseo@gmail.com>
2018-01-15 14:26:21 +0800

    update ping

--- app/modules/test/ping.lua
+++ app/modules/test/ping.lua
@@ -2,19 +2,20 @@
 local HTTP_INTERNAL_SERVER_ERROR = ngx.HTTP_INTERNAL_SERVER_ERROR
 local HTTP_CREATED = ngx.HTTP_CREATED
 -- include
+local cstDef = require("src.define.const")
 local msgDef = require("src.define.message")
-local dispatch = require("modules.dispatcher")
+local schedule = require("scheduler.index")
 
 return function()
     return function(req, res)
+        local mode = cstDef.DISPATCH_MODE.TCP
 		local pingReq = {
 		    id = msgDef.MESSAGE_PING,
 		    body = {
 			    seq = req.query.seq
 		    }
 	    }
-		local serverName = "testServer1"
-		local pingResp = dispatch(serverName, pingReq)
+		local pingResp = schedule(mode, {"testServer1", pingReq})
 		if not pingResp then
 			res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
 			return
