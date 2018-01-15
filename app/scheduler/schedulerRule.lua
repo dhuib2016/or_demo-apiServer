d1ad18b15eec3c4780e59dc0458efffc899bb128
tweyseo <tweyseo@gmail.com>
2018-01-15 14:29:56 +0800

    fix route err

--- app/scheduler/schedulerRule.lua
+++ app/scheduler/schedulerRule.lua
@@ -3,6 +3,6 @@ local httpDispatcher = require("scheduler.dispatcher.message.http")
 local tcpDispatcher = require("scheduler.dispatcher.message.tcp")
 
 return {
-	[cstDef.DISPATCH_MODE.HTTP] = httpDispatcher,
-	[cstDef.DISPATCH_MODE.TCP] = tcpDispatcher
+	[cstDef.DISPATCH_MODE.MESSAGE.HTTP] = httpDispatcher,
+	[cstDef.DISPATCH_MODE.MESSAGE.TCP] = tcpDispatcher
 }
