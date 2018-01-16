9a44065b1e39002c6caaceb5bc5e273c90242d61
tweyseo <tweyseo@gmail.com>
2018-01-16 11:12:12 +0800

    add lua

--- app/routes/user.lua
+++ app/routes/user.lua
@@ -1,10 +1,9 @@
 local lor = require("lor.index")
 local userRouter = lor:Router()
 local create = require("modules.user.create")
---local query = require("modules.user.create")
+local query = require("modules.user.query")
 
---userRouter:post("/create", creator:createId(), creator:createName())
 userRouter:post("/create", create())
---userRouter:get("/query", query())
+userRouter:get("/query", query())
 
 return userRouter
