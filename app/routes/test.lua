30f7a4e4c49380d4d98f33500e0a32fcdee4dffc
tweyseo <tweyseo@gmail.com>
2018-01-19 11:52:09 +0800

    add httpPingPro test for restyhttp

--- app/routes/test.lua
+++ app/routes/test.lua
@@ -3,12 +3,14 @@ local testRouter = lor:Router()
 local hello = require("modules.test.hello")
 local ping = require("modules.test.ping")
 local httpPing = require("modules.test.httpPing")
+local httpPingPro = require("modules.test.httpPingPro")
 
 
 testRouter:get("/hello", hello())
 
 testRouter:get("/ping", ping())
 testRouter:get("/httpPing", httpPing())
+testRouter:get("/httpPingPro", httpPingPro())
 
 
 return testRouter
