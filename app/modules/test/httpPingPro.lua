51959bf13a45469f6f3937f15c20dc4923a17bb6
tweyseo <tweyseo@gmail.com>
2018-01-19 11:50:23 +0800

    add restyhttp

---
+++ app/modules/test/httpPingPro.lua
@@ -0,0 +1,57 @@
+-- function reference
+local log = ngx.log
+local WARN = ngx.WARN
+local HTTP_INTERNAL_SERVER_ERROR = ngx.HTTP_INTERNAL_SERVER_ERROR
+local HTTP_OK = ngx.HTTP_OK
+local http = require("resty.http")
+
+-- include
+--local cstDef = require("src.define.const")
+--local msgDef = require("src.define.message")
+--local schedule = require("scheduler.index")
+--local ec = require("src.define.errorCode")
+
+return function()
+    return function(_, res)
+        -- local mode = cstDef.DISPATCH_MODE.MESSAGE.TCP
+		-- local content = {}
+        -- content.request = {
+		    -- id = msgDef.MESSAGE_PING,
+		    -- body = {
+			    -- seq = req.query.seq
+		    -- }
+	    -- }
+        -- content.serverName = "httpTestServer"
+		-- local pingResp = schedule(mode, content)
+		-- if not pingResp then
+			-- res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
+			-- return
+		-- end
+
+        ngx.req.read_body()
+        local args, err = ngx.req.get_uri_args()
+        if not args then
+            log(WARN, "get uri args failed: ", err)
+            res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
+            return
+        end
+
+        local httpc = http.new()
+        local uri = "http://127.0.0.1:29527/ping"
+        local pingResp, pingErr = httpc:request_uri(uri, { args = args })
+        if not pingResp then
+            log(WARN, "ping failed: ", pingErr)
+            res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
+            return
+        end
+
+        local status = pingResp.status
+        if status ~= HTTP_OK then
+            log(WARN, "ping failed: ", status)
+            res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
+            return
+        end
+
+	    res:status(HTTP_OK):send(pingResp.body)
+    end
+end
