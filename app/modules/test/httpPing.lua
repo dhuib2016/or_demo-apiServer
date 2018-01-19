e8914d763cbf6cfc0255901aeda912693a4f579d
tweyseo <tweyseo@gmail.com>
2018-01-19 15:59:05 +0800

    http test

--- app/modules/test/httpPing.lua
+++ app/modules/test/httpPing.lua
@@ -3,39 +3,12 @@ local log = ngx.log
 local WARN = ngx.WARN
 local HTTP_INTERNAL_SERVER_ERROR = ngx.HTTP_INTERNAL_SERVER_ERROR
 local HTTP_OK = ngx.HTTP_OK
+local get_uri_args = ngx.req.get_uri_args
 local capture = ngx.location.capture
--- include
---local cstDef = require("src.define.const")
---local msgDef = require("src.define.message")
---local schedule = require("scheduler.index")
---local ec = require("src.define.errorCode")
 
 return function()
-    return function(_, res)
-        -- local mode = cstDef.DISPATCH_MODE.MESSAGE.TCP
-		-- local content = {}
-        -- content.request = {
-		    -- id = msgDef.MESSAGE_PING,
-		    -- body = {
-			    -- seq = req.query.seq
-		    -- }
-	    -- }
-        -- content.serverName = "httpTestServer"
-		-- local pingResp = schedule(mode, content)
-		-- if not pingResp then
-			-- res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
-			-- return
-		-- end
-
-        ngx.req.read_body()
-        local args, err = ngx.req.get_uri_args()
-        if not args then
-            log(WARN, "get uri args failed: ", err)
-            res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
-            return
-        end
-
-        local pingResp = capture("/ping", { args = args })
+    return function(req, res)
+        local pingResp = capture("/ping", { args = req.query })
         local status = pingResp.status
         if status ~= HTTP_OK then
             log(WARN, "capture failed: ", status)
--- app/modules/test/httpPingPro.lua
+++ app/modules/test/httpPingPro.lua
@@ -5,40 +5,11 @@ local HTTP_INTERNAL_SERVER_ERROR = ngx.HTTP_INTERNAL_SERVER_ERROR
 local HTTP_OK = ngx.HTTP_OK
 local http = require("resty.http")
 
--- include
---local cstDef = require("src.define.const")
---local msgDef = require("src.define.message")
---local schedule = require("scheduler.index")
---local ec = require("src.define.errorCode")
-
 return function()
-    return function(_, res)
-        -- local mode = cstDef.DISPATCH_MODE.MESSAGE.TCP
-		-- local content = {}
-        -- content.request = {
-		    -- id = msgDef.MESSAGE_PING,
-		    -- body = {
-			    -- seq = req.query.seq
-		    -- }
-	    -- }
-        -- content.serverName = "httpTestServer"
-		-- local pingResp = schedule(mode, content)
-		-- if not pingResp then
-			-- res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
-			-- return
-		-- end
-
-        ngx.req.read_body()
-        local args, err = ngx.req.get_uri_args()
-        if not args then
-            log(WARN, "get uri args failed: ", err)
-            res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
-            return
-        end
-
+    return function(req, res)
         local httpc = http.new()
         local uri = "http://127.0.0.1:29527/ping"
-        local params = { body = "seq = "..args.seq }
+        local params = { body = "seq = "..req.query.seq }
         local pingResp, pingErr = httpc:request_uri(uri, params)
         if not pingResp then
             log(WARN, "ping failed: ", pingErr)
