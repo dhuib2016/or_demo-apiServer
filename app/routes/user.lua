261ea51055d422a3e1c3a70079d9e71a7c903d1b
tweyseo <tweyseo@gmail.com>
2018-01-22 11:48:34 +0800

    add httpCreatePar route

--- app/routes/user.lua
+++ app/routes/user.lua
@@ -4,10 +4,12 @@ local create = require("modules.user.create")
 local createPar = require("modules.user.createPar")
 local httpCreate = require("modules.user.httpCreate")
 local httpCreatePro = require("modules.user.httpCreatePro")
+local httpCreatePar = require("modules.user.httpCreatePar")
 
 userRouter:post("/create", create())
 userRouter:post("/createPar", createPar())
 userRouter:post("/httpCreate", httpCreate())
 userRouter:post("/httpCreatePro", httpCreatePro())
+userRouter:post("/httpCreatePar", httpCreatePar())
 
 return userRouter
