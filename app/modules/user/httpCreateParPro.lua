972bd7a3fe609cf0a7cf1397c3aeb1d568451638
tweyseo <tweyseo@gmail.com>
2018-01-22 17:47:43 +0800

    captureMulti test

--- app/modules/user/httpCreateParPro.lua
+++ app/modules/user/httpCreateParPro.lua
@@ -12,8 +12,8 @@ return function()
         local contents = {}
 
         local c1 = {}
-        c1.uri = "http://127.0.0.1:29528/createId"
-        c1.request = {
+        c1[1] = "http://127.0.0.1:29528/createId"
+        c1[2] = {
             method = "POST",
             body = "id = "..params.id
         }
@@ -21,8 +21,8 @@ return function()
         contents[createIdIndex] = c1
 
         local c2 = {}
-        c2.uri = "http://127.0.0.1:29529/createName"
-        c2.request= {
+        c2[1] = "http://127.0.0.1:29529/createName"
+        c2[2]= {
             method = "POST",
             args = "name = "..params.name
         }
--- app/scheduler/dispatcher/message/captureMulti.lua
+++ app/scheduler/dispatcher/message/captureMulti.lua
@@ -1,16 +1,22 @@
 -- function reference
+local type = type
 local log = ngx.log
 local WARN = ngx.WARN
 local HTTP_OK = ngx.HTTP_OK
 local capture_multi = ngx.location.capture_multi
 
 return function(params)
+    if type(params) ~= "table" or #params < 2 then
+        log(WARN, "invalid params(params must be an array-table and length >= 2)")
+        return nil
+    end
+
 	local rets = capture_multi(params)
     local resps = {}
     for i, ret in ipairs(rets) do
         local status = ret.status
         if status ~= HTTP_OK then
-            log(WARN, "capture failed: ", status)
+            log(WARN, "capture multi failed: ", status)
             resps[i] = 0
         else
             resps[i] = ret.body
