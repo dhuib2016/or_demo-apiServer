220522377309d0ca3b6c8231d2c9cf43a6f2f956
tweyseo <tweyseo@gmail.com>
2018-01-22 17:36:41 +0800

    add parpro route

--- app/routes/user.lua
+++ app/routes/user.lua
@@ -5,11 +5,13 @@ local createPar = require("modules.user.createPar")
 local httpCreate = require("modules.user.httpCreate")
 local httpCreatePro = require("modules.user.httpCreatePro")
 local httpCreatePar = require("modules.user.httpCreatePar")
+local httpCreateParPro = require("modules.user.httpCreateParPro")
 
 userRouter:post("/create", create())
 userRouter:post("/createPar", createPar())
 userRouter:post("/httpCreate", httpCreate())
 userRouter:post("/httpCreatePro", httpCreatePro())
 userRouter:post("/httpCreatePar", httpCreatePar())
+userRouter:post("/httpCreateParPro", httpCreateParPro())
 
 return userRouter
