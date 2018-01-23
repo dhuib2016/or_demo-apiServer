cf6a13ca076f69488b13e5f40ee0d252fad19992
tweyseo <tweyseo@gmail.com>
2018-01-23 15:06:38 +0800

    keep alive conf

--- app/modules/test/httpPing.lua
+++ app/modules/test/httpPing.lua
@@ -1,30 +1,25 @@
 -- function reference
-local log = ngx.log
-local WARN = ngx.WARN
 local HTTP_INTERNAL_SERVER_ERROR = ngx.HTTP_INTERNAL_SERVER_ERROR
 local HTTP_OK = ngx.HTTP_OK
 --include
-local http = require("resty.http")
+local cstDef = require("src.define.const")
+local schedule = require("scheduler.index")
 
 return function()
     return function(req, res)
-        local httpc = http.new()
-        local uri = "http://127.0.0.1:29527/ping"
-        local params = { body = "seq = "..req.query.seq }
-        local pingResp, pingErr = httpc:request_uri(uri, params)
+        local mode = cstDef.DISPATCH_MODE.MESSAGE.HTTP
+        local params = req.query
+        
+        local content = {}
+        content.uri = "http://127.0.0.1:29527/ping"
+        content.params = { body = "seq = "..params.seq }
+        
+        local pingResp = schedule(mode, content)
         if not pingResp then
-            log(WARN, "ping failed: ", pingErr)
             res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
             return
         end
 
-        local status = pingResp.status
-        if status ~= HTTP_OK then
-            log(WARN, "ping failed: ", status)
-            res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
-            return
-        end
-
-	    res:status(HTTP_OK):send(pingResp.body)
+	    res:status(HTTP_OK):send(pingResp)
     end
 end
--- app/modules/test/httpPingPro.lua
+++ app/modules/test/httpPingPro.lua
@@ -1,20 +1,27 @@
 -- function reference
-local log = ngx.log
-local WARN = ngx.WARN
 local HTTP_INTERNAL_SERVER_ERROR = ngx.HTTP_INTERNAL_SERVER_ERROR
 local HTTP_OK = ngx.HTTP_OK
-local capture = ngx.location.capture
+local HTTP_POST = ngx.HTTP_POST
+-- include
+local cstDef = require("src.define.const")
+local schedule = require("scheduler.index")
 
 return function()
     return function(req, res)
-        local pingResp = capture("/ping", { args = req.query })
-        local status = pingResp.status
-        if status ~= HTTP_OK then
-            log(WARN, "capture ping failed: ", status)
-            res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
-            return
-        end
+        local mode = cstDef.DISPATCH_MODE.MESSAGE.CAPTURE
+        
+        local content = {}
+        content.uri = "/ping"
+        content.request = {
+            method = HTTP_POST,
+            args = req.query
+        }
+        local pingResp = schedule(mode, content)
+		if not pingResp then
+			res:status(HTTP_INTERNAL_SERVER_ERROR):send("ping failed!")
+			return
+		end
 
-	    res:status(HTTP_OK):send(pingResp.body)
+	    res:status(HTTP_OK):send(pingResp)
     end
 end
