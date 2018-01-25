94cc3fb679b55029113e8eca5f13d0ea14acffd3
tweyseo <tweyseo@gmail.com>
2018-01-25 10:11:34 +0800

    revert route tree

--- app/router.lua
+++ app/router.lua
@@ -8,15 +8,15 @@ return function(app)
         res:send("welcome to OR framework.")
     end)
 
-    -- -- todo:auto read files and require them under routes/
-	-- -- test
-	-- -- group router mapping a coarse-grained client request
-	-- app:use("/test", testRouter())
-	-- --a default error handler for a group router
-	-- app:erruse("/test", testErrHandler())
+    -- todo:auto read files and require them under routes/
+	-- test
+	-- group router mapping a coarse-grained client request
+	app:use("/test", testRouter())
+	--a default error handler for a group router
+	app:erruse("/test", testErrHandler())
 
     -- do not use route tree
-    testRouter(app, "/test")
-    app:erruse("/test", testErrHandler())
+    -- testRouter(app, "/test")
+    -- app:erruse("/test", testErrHandler())
 end
 
--- app/routes/test/index.lua
+++ app/routes/test/index.lua
@@ -1,32 +1,3 @@
-local hello = require("routes.test.hello")
-local ping = require("routes.test.ping")
-local httpPing = require("routes.test.httpPing")
-local httpPingPro = require("routes.test.httpPingPro")
-local create = require("routes.test.create")
-local createPar = require("routes.test.createPar")
-local httpCreate = require("routes.test.httpCreate")
-local httpCreatePro = require("routes.test.httpCreatePro")
-local httpCreatePar = require("routes.test.httpCreatePar")
-local httpCreateParPro = require("routes.test.httpCreateParPro")
-
-return function(app, path)
-    -- path under /test
-    app:get(path.."/hello", hello())
-
-    app:get(path.."/ping", ping())
-    app:get(path.."/httpPing", httpPing())
-    app:get(path.."/httpPingPro", httpPingPro())
-
-    app:post(path.."/create", create())
-    app:post(path.."/httpCreate", httpCreate())
-    app:post(path.."/httpCreatePro", httpCreatePro())
-
-    app:post(path.."/createPar", createPar())
-    app:post(path.."/httpCreatePar", httpCreatePar())
-    app:post(path.."/httpCreateParPro", httpCreateParPro())
-end
--- local lor = require("lor.index")
--- local testRouter = lor:Router()
 -- local hello = require("routes.test.hello")
 -- local ping = require("routes.test.ping")
 -- local httpPing = require("routes.test.httpPing")
@@ -38,19 +9,48 @@ end
 -- local httpCreatePar = require("routes.test.httpCreatePar")
 -- local httpCreateParPro = require("routes.test.httpCreateParPro")
 
+-- return function(app, path)
+    -- -- path under /test
+    -- app:get(path.."/hello", hello())
 
--- testRouter:get("/hello", hello())
+    -- app:get(path.."/ping", ping())
+    -- app:get(path.."/httpPing", httpPing())
+    -- app:get(path.."/httpPingPro", httpPingPro())
 
--- testRouter:get("/ping", ping())
--- testRouter:get("/httpPing", httpPing())
--- testRouter:get("/httpPingPro", httpPingPro())
+    -- app:post(path.."/create", create())
+    -- app:post(path.."/httpCreate", httpCreate())
+    -- app:post(path.."/httpCreatePro", httpCreatePro())
 
--- testRouter:post("/create", create())
--- testRouter:post("/httpCreate", httpCreate())
--- testRouter:post("/httpCreatePro", httpCreatePro())
+    -- app:post(path.."/createPar", createPar())
+    -- app:post(path.."/httpCreatePar", httpCreatePar())
+    -- app:post(path.."/httpCreateParPro", httpCreateParPro())
+-- end
+local lor = require("lor.index")
+local testRouter = lor:Router()
+local hello = require("routes.test.hello")
+local ping = require("routes.test.ping")
+local httpPing = require("routes.test.httpPing")
+local httpPingPro = require("routes.test.httpPingPro")
+local create = require("routes.test.create")
+local createPar = require("routes.test.createPar")
+local httpCreate = require("routes.test.httpCreate")
+local httpCreatePro = require("routes.test.httpCreatePro")
+local httpCreatePar = require("routes.test.httpCreatePar")
+local httpCreateParPro = require("routes.test.httpCreateParPro")
 
--- testRouter:post("/createPar", createPar())
--- testRouter:post("/httpCreatePar", httpCreatePar())
--- testRouter:post("/httpCreateParPro", httpCreateParPro())
 
--- return testRouter
+testRouter:get("/hello", hello())
+
+testRouter:get("/ping", ping())
+testRouter:get("/httpPing", httpPing())
+testRouter:get("/httpPingPro", httpPingPro())
+
+testRouter:post("/create", create())
+testRouter:post("/httpCreate", httpCreate())
+testRouter:post("/httpCreatePro", httpCreatePro())
+
+testRouter:post("/createPar", createPar())
+testRouter:post("/httpCreatePar", httpCreatePar())
+testRouter:post("/httpCreateParPro", httpCreateParPro())
+
+return testRouter
