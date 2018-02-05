5480d2314ad0e9e0845c0da3d2b39ee23d3fa075
tweyseo <tweyseo@gmail.com>
2018-02-05 14:46:13 +0800

    fix path bug

--- app/handlers/test/index.lua
+++ app/handlers/test/index.lua
@@ -12,7 +12,7 @@
 local autoRequire = require("handlers.autoRequire")
 
 return function(app)
-    local requireTable = autoRequire("/app/handlers/test/")
+    local requireTable = autoRequire("/app/handlers/test")
 
     app:get("/test/hello", requireTable.hello())
 
